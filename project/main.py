5ab5932fcabdeb750d80e06ba4c8d45b73da71c5
dongho1 <dongho108@naver.com>
2020-11-13 23:30:54 +0900

    before 3) from window / need nltk(corpura/stopwords), lxml in osx

--- project/main.py
+++ project/main.py
@@ -12,7 +12,7 @@ np.random.seed(seed=0)
 
 
 #data load
-data = pd.read_csv("Reviews.csv", nrows = 100000)
+data = pd.read_csv("Reviews.csv", nrows = 1000)
 data = data[['Text', 'Summary']]
 
 #data refine
@@ -20,9 +20,133 @@ data.drop_duplicates(subset=['Text'], inplace=True)
 data.dropna(axis=0, inplace=True)
 
 #nltk stop word
-nltk.download()
 stop_words = set(stopwords.words('english'))
-print('불용어 개수 :', len(stop_words))
-print(stop_words)
 
 
+def preprocess_sentence(sentence, remove_stopwords = True):
+    contractions = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
+                    "could've": "could have", "couldn't": "could not", "didn't": "did not", "doesn't": "does not",
+                    "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+                    "he'd": "he would", "he'll": "he will", "he's": "he is", "how'd": "how did",
+                    "how'd'y": "how do you", "how'll": "how will", "how's": "how is", "I'd": "I would",
+                    "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have", "I'm": "I am",
+                    "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",
+                    "i'll've": "i will have", "i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+                    "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have", "it's": "it is",
+                    "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have",
+                    "mightn't": "might not", "mightn't've": "might not have", "must've": "must have",
+                    "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not",
+                    "needn't've": "need not have", "o'clock": "of the clock", "oughtn't": "ought not",
+                    "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not",
+                    "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have",
+                    "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have",
+                    "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have", "so's": "so as",
+                    "this's": "this is", "that'd": "that would", "that'd've": "that would have", "that's": "that is",
+                    "there'd": "there would", "there'd've": "there would have", "there's": "there is",
+                    "here's": "here is", "they'd": "they would", "they'd've": "they would have", "they'll": "they will",
+                    "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have",
+                    "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will",
+                    "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not",
+                    "what'll": "what will", "what'll've": "what will have", "what're": "what are", "what's": "what is",
+                    "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did",
+                    "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have",
+                    "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have",
+                    "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have",
+                    "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
+                    "y'all'd": "you all would", "y'all'd've": "you all would have", "y'all're": "you all are",
+                    "y'all've": "you all have", "you'd": "you would", "you'd've": "you would have",
+                    "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+
+    sentence = sentence.lower() # 텍스트 소문자화
+    sentence = BeautifulSoup(sentence, "lxml").text  # <br />, <a href = ...> 등의 html 태그 제거
+    sentence = re.sub(r'\([^)]*\)', '', sentence)  # 괄호로 닫힌 문자열  제거 Ex) my husband (and myself) for => my husband for
+    sentence = re.sub('"','', sentence)  # 쌍따옴표 " 제거
+    sentence = ' '.join([contractions[t] if t in contractions else t for t in sentence.split(" ")])  # 약어 정규화
+    sentence = re.sub(r"'s\b","",sentence)  # 소유격 제거. Ex) roland's -> roland
+    sentence = re.sub("[^a-zA-Z]", " ", sentence)  # 영어 외 문자(숫자, 특수문자 등) 공백으로 변환
+    sentence = re.sub('[m]{2,}', 'mm', sentence)  # m이 3개 이상이면 2개로 변경. Ex) ummmmmmm yeah -> umm yeah
+
+    # 불용어 제거 (Text)
+    if remove_stopwords:
+        tokens = ' '.join(word for word in sentence.split() if not word in stop_words if len(word) > 1)
+    # 불용어 미제거 (Summary)
+    else:
+        tokens = ' '.join(word for word in sentence.split() if len(word) > 1)
+    return tokens
+
+temp_text = 'Everything I bought was great, infact I ordered twice and the third ordered was<br />for my mother and father.'
+temp_summary = 'Great way to start (or finish) the day!!!'
+
+
+# Text 열 전처리
+clean_text = []
+for s in data['Text']:
+    clean_text.append(preprocess_sentence(s))
+
+# Summary 열 전처리
+clean_summary = []
+for s in data['Summary']:
+    clean_summary.append(preprocess_sentence(s, 0))
+
+data['Text'] = clean_text
+data['Summary'] = clean_summary
+
+# 길이가 공백인 샘플은 NULL 값으로 변환
+data.replace('', np.nan, inplace=True)
+
+data.dropna(axis = 0, inplace = True)
+print('전체 샘플수 :',(len(data)))
+
+# 길이 분포 출력
+text_len = [len(s.split()) for s in data['Text']]
+summary_len = [len(s.split()) for s in data['Summary']]
+
+print('텍스트의 최소 길이 : {}'.format(np.min(text_len)))
+print('텍스트의 최대 길이 : {}'.format(np.max(text_len)))
+print('텍스트의 평균 길이 : {}'.format(np.mean(text_len)))
+print('요약의 최소 길이 : {}'.format(np.min(summary_len)))
+print('요약의 최대 길이 : {}'.format(np.max(summary_len)))
+print('요약의 평균 길이 : {}'.format(np.mean(summary_len)))
+
+plt.subplot(1,2,1)
+plt.boxplot(summary_len)
+plt.title('Summary')
+plt.subplot(1,2,2)
+plt.boxplot(text_len)
+plt.title('Text')
+plt.tight_layout()
+plt.show()
+
+plt.title('Summary')
+plt.hist(summary_len, bins=40)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+plt.title('Text')
+plt.hist(text_len, bins=40)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+text_max_len = 50
+summary_max_len = 8
+
+def below_threshold_len(max_len, nested_list):
+  cnt = 0
+  for s in nested_list:
+    if(len(s.split()) <= max_len):
+        cnt = cnt + 1
+  print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (cnt / len(nested_list))))
+
+below_threshold_len(text_max_len, data['Text'])
+below_threshold_len(summary_max_len, data['Summary'])
+
+data = data[data['Text'].apply(lambda x: len(x.split()) <= text_max_len)]
+data = data[data['Summary'].apply(lambda x: len(x.split()) <= summary_max_len)]
+print('전체 샘플수 :',(len(data)))
+print(data.head())
+
+encoder_input = np.array(data['Text'])
+decoder_input = np.array(data['decoder_input'])
+decoder_target = np.array(data['decoder_target'])
