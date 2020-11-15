2eca9072bb7904df27b758f014e82a505c681d30
dongho1 <dongho108@naver.com>
2020-11-16 00:54:48 +0900

    beta 1.0 from window / need nltk(corpura/stopwords), lxml in osx

--- project/main.py
+++ project/main.py
@@ -7,10 +7,19 @@ from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from attention import AttentionLayer
 import urllib.request
+
+
+
+
+
+##************** data refactoring ****************
+
 np.random.seed(seed=0)
-
-
 #data load
 data = pd.read_csv("Reviews.csv", nrows = 1000)
 data = data[['Text', 'Summary']]
@@ -95,39 +104,39 @@ data['Summary'] = clean_summary
 data.replace('', np.nan, inplace=True)
 
 data.dropna(axis = 0, inplace = True)
-print('전체 샘플수 :',(len(data)))
+# print('전체 샘플수 :',(len(data)))
 
 # 길이 분포 출력
 text_len = [len(s.split()) for s in data['Text']]
 summary_len = [len(s.split()) for s in data['Summary']]
 
-print('텍스트의 최소 길이 : {}'.format(np.min(text_len)))
-print('텍스트의 최대 길이 : {}'.format(np.max(text_len)))
-print('텍스트의 평균 길이 : {}'.format(np.mean(text_len)))
-print('요약의 최소 길이 : {}'.format(np.min(summary_len)))
-print('요약의 최대 길이 : {}'.format(np.max(summary_len)))
-print('요약의 평균 길이 : {}'.format(np.mean(summary_len)))
+# print('텍스트의 최소 길이 : {}'.format(np.min(text_len)))
+# print('텍스트의 최대 길이 : {}'.format(np.max(text_len)))
+# print('텍스트의 평균 길이 : {}'.format(np.mean(text_len)))
+# print('요약의 최소 길이 : {}'.format(np.min(summary_len)))
+# print('요약의 최대 길이 : {}'.format(np.max(summary_len)))
+# print('요약의 평균 길이 : {}'.format(np.mean(summary_len)))
 
-plt.subplot(1,2,1)
-plt.boxplot(summary_len)
-plt.title('Summary')
-plt.subplot(1,2,2)
-plt.boxplot(text_len)
-plt.title('Text')
-plt.tight_layout()
-plt.show()
+# plt.subplot(1,2,1)
+# plt.boxplot(summary_len)
+# plt.title('Summary')
+# plt.subplot(1,2,2)
+# plt.boxplot(text_len)
+# plt.title('Text')
+# plt.tight_layout()
+# plt.show()
 
-plt.title('Summary')
-plt.hist(summary_len, bins=40)
-plt.xlabel('length of samples')
-plt.ylabel('number of samples')
-plt.show()
+# plt.title('Summary')
+# plt.hist(summary_len, bins=40)
+# plt.xlabel('length of samples')
+# plt.ylabel('number of samples')
+# plt.show()
 
-plt.title('Text')
-plt.hist(text_len, bins=40)
-plt.xlabel('length of samples')
-plt.ylabel('number of samples')
-plt.show()
+# plt.title('Text')
+# plt.hist(text_len, bins=40)
+# plt.xlabel('length of samples')
+# plt.ylabel('number of samples')
+# plt.show()
 
 text_max_len = 50
 summary_max_len = 8
@@ -137,16 +146,297 @@ def below_threshold_len(max_len, nested_list):
   for s in nested_list:
     if(len(s.split()) <= max_len):
         cnt = cnt + 1
-  print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (cnt / len(nested_list))))
+  # print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (cnt / len(nested_list))))
 
 below_threshold_len(text_max_len, data['Text'])
 below_threshold_len(summary_max_len, data['Summary'])
 
 data = data[data['Text'].apply(lambda x: len(x.split()) <= text_max_len)]
 data = data[data['Summary'].apply(lambda x: len(x.split()) <= summary_max_len)]
-print('전체 샘플수 :',(len(data)))
-print(data.head())
+# print('전체 샘플수 :',(len(data)))
+
+# 요약 데이터에는 시작 토큰과 종료 토큰을 추가한다.
+data['decoder_input'] = data['Summary'].apply(lambda x : 'sostoken '+ x)
+data['decoder_target'] = data['Summary'].apply(lambda x : x + ' eostoken')
+
+# print(data.head())
 
 encoder_input = np.array(data['Text'])
 decoder_input = np.array(data['decoder_input'])
 decoder_target = np.array(data['decoder_target'])
+
+indices = np.arange(encoder_input.shape[0])
+np.random.shuffle(indices)
+# print(indices)
+
+n_of_val = int(len(encoder_input)*0.2)
+# print('테스트 데이터의 수 :',n_of_val)
+
+encoder_input_train = encoder_input[:-n_of_val]
+decoder_input_train = decoder_input[:-n_of_val]
+decoder_target_train = decoder_target[:-n_of_val]
+
+encoder_input_test = encoder_input[-n_of_val:]
+decoder_input_test = decoder_input[-n_of_val:]
+decoder_target_test = decoder_target[-n_of_val:]
+
+# print('훈련 데이터의 개수 :', len(encoder_input_train))
+# print('훈련 레이블의 개수 :',len(decoder_input_train))
+# print('테스트 데이터의 개수 :',len(encoder_input_test))
+# print('테스트 레이블의 개수 :',len(decoder_input_test))
+
+src_tokenizer = Tokenizer()
+src_tokenizer.fit_on_texts(encoder_input_train)
+
+threshold = 7
+total_cnt = len(src_tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in src_tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+# print('단어 집합(vocabulary)의 크기 :',total_cnt)
+# print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+# print('단어 집합에서 희귀 단어를 제외시킬 경우의 단어 집합의 크기 %s'%(total_cnt - rare_cnt))
+# print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+# print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+src_vocab = 8000
+src_tokenizer = Tokenizer(num_words = src_vocab)
+src_tokenizer.fit_on_texts(encoder_input_train)
+
+# 텍스트 시퀀스를 정수 시퀀스로 변환
+encoder_input_train = src_tokenizer.texts_to_sequences(encoder_input_train)
+encoder_input_test = src_tokenizer.texts_to_sequences(encoder_input_test)
+
+# print(encoder_input_train[:3])
+
+tar_tokenizer = Tokenizer()
+tar_tokenizer.fit_on_texts(decoder_input_train)
+
+threshold = 6
+total_cnt = len(tar_tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tar_tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+# print('단어 집합(vocabulary)의 크기 :',total_cnt)
+# print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+# print('단어 집합에서 희귀 단어를 제외시킬 경우의 단어 집합의 크기 %s'%(total_cnt - rare_cnt))
+# print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+# print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+tar_vocab = 2000
+tar_tokenizer = Tokenizer(num_words = tar_vocab)
+tar_tokenizer.fit_on_texts(decoder_input_train)
+tar_tokenizer.fit_on_texts(decoder_target_train)
+
+# 텍스트 시퀀스를 정수 시퀀스로 변환
+decoder_input_train = tar_tokenizer.texts_to_sequences(decoder_input_train)
+decoder_target_train = tar_tokenizer.texts_to_sequences(decoder_target_train)
+decoder_input_test = tar_tokenizer.texts_to_sequences(decoder_input_test)
+decoder_target_test = tar_tokenizer.texts_to_sequences(decoder_target_test)
+
+# print(decoder_input_train[:5])
+# print(decoder_target_train[:5])
+
+drop_train = [index for index, sentence in enumerate(decoder_input_train) if len(sentence) == 1]
+drop_test = [index for index, sentence in enumerate(decoder_input_test) if len(sentence) == 1]
+
+# print('삭제할 훈련 데이터의 개수 :',len(drop_train))
+# print('삭제할 테스트 데이터의 개수 :',len(drop_test))
+
+encoder_input_train = np.delete(encoder_input_train, drop_train, axis=0)
+decoder_input_train = np.delete(decoder_input_train, drop_train, axis=0)
+decoder_target_train = np.delete(decoder_target_train, drop_train, axis=0)
+
+encoder_input_test = np.delete(encoder_input_test, drop_test, axis=0)
+decoder_input_test = np.delete(decoder_input_test, drop_test, axis=0)
+decoder_target_test = np.delete(decoder_target_test, drop_test, axis=0)
+
+# print('훈련 데이터의 개수 :', len(encoder_input_train))
+# print('훈련 레이블의 개수 :',len(decoder_input_train))
+# print('테스트 데이터의 개수 :',len(encoder_input_test))
+# print('테스트 레이블의 개수 :',len(decoder_input_test))
+
+encoder_input_train = pad_sequences(encoder_input_train, maxlen = text_max_len, padding='post')
+encoder_input_test = pad_sequences(encoder_input_test, maxlen = text_max_len, padding='post')
+decoder_input_train = pad_sequences(decoder_input_train, maxlen = summary_max_len, padding='post')
+decoder_target_train = pad_sequences(decoder_target_train, maxlen = summary_max_len, padding='post')
+decoder_input_test = pad_sequences(decoder_input_test, maxlen = summary_max_len, padding='post')
+decoder_target_test = pad_sequences(decoder_target_test, maxlen = summary_max_len, padding='post')
+
+
+# ************* seq2seq + attetion training **************
+
+embedding_dim = 128
+hidden_size = 256
+
+# 인코더
+encoder_inputs = Input(shape=(text_max_len,))
+
+# 인코더의 임베딩 층
+enc_emb = Embedding(src_vocab, embedding_dim)(encoder_inputs)
+
+# 인코더의 LSTM 1
+encoder_lstm1 = LSTM(hidden_size, return_sequences=True, return_state=True ,dropout = 0.4, recurrent_dropout = 0.4)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+
+# 인코더의 LSTM 2
+encoder_lstm2 = LSTM(hidden_size, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+# 인코더의 LSTM 3
+encoder_lstm3 = LSTM(hidden_size, return_state=True, return_sequences=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
+
+# 디코더
+decoder_inputs = Input(shape=(None,))
+
+# 디코더의 임베딩 층
+dec_emb_layer = Embedding(tar_vocab, embedding_dim)
+dec_emb = dec_emb_layer(decoder_inputs)
+
+# 디코더의 LSTM
+decoder_lstm = LSTM(hidden_size, return_sequences = True, return_state = True, dropout = 0.4, recurrent_dropout=0.2)
+decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state = [state_h, state_c])
+
+# 디코더의 출력층
+decoder_softmax_layer = Dense(tar_vocab, activation = 'softmax')
+decoder_softmax_outputs = decoder_softmax_layer(decoder_outputs)
+
+# 모델 정의
+model = Model([encoder_inputs, decoder_inputs], decoder_softmax_outputs)
+# print(model.summary())
+
+# 어텐션 층(어텐션 함수)
+attn_layer = AttentionLayer(name='attention_layer')
+attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+
+# 어텐션의 결과와 디코더의 hidden state들을 연결
+decoder_concat_input = Concatenate(axis = -1, name='concat_layer')([decoder_outputs, attn_out])
+
+# 디코더의 출력층
+decoder_softmax_layer = Dense(tar_vocab, activation='softmax')
+decoder_softmax_outputs = decoder_softmax_layer(decoder_concat_input)
+
+# 모델 정의
+model = Model([encoder_inputs, decoder_inputs], decoder_softmax_outputs)
+# print(model.summary())
+
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience = 2)
+history = model.fit(x = [encoder_input_train, decoder_input_train], y = decoder_target_train, \
+          validation_data = ([encoder_input_test, decoder_input_test], decoder_target_test),
+          batch_size = 256, callbacks=[es], epochs = 50)
+
+# plt.plot(history.history['loss'], label='train')
+# plt.plot(history.history['val_loss'], label='test')
+# plt.legend()
+# plt.show()
+
+
+
+
+# 모델 테스트
+src_index_to_word = src_tokenizer.index_word # 원문 단어 집합에서 정수 -> 단어를 얻음
+tar_word_to_index = tar_tokenizer.word_index # 요약 단어 집합에서 단어 -> 정수를 얻음
+tar_index_to_word = tar_tokenizer.index_word # 요약 단어 집합에서 정수 -> 단어를 얻음
+
+# 인코더 설계
+encoder_model = Model(inputs=encoder_inputs, outputs=[encoder_outputs, state_h, state_c])
+
+# 이전 시점의 상태들을 저장하는 텐서
+decoder_state_input_h = Input(shape=(hidden_size,))
+decoder_state_input_c = Input(shape=(hidden_size,))
+
+dec_emb2 = dec_emb_layer(decoder_inputs)
+# 문장의 다음 단어를 예측하기 위해서 초기 상태(initial_state)를 이전 시점의 상태로 사용. 이는 뒤의 함수 decode_sequence()에 구현
+# 훈련 과정에서와 달리 LSTM의 리턴하는 은닉 상태와 셀 상태인 state_h와 state_c를 버리지 않음.
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+
+# 어텐션 함수
+decoder_hidden_state_input = Input(shape=(text_max_len, hidden_size))
+attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+# 디코더의 출력층
+decoder_outputs2 = decoder_softmax_layer(decoder_inf_concat)
+
+# 최종 디코더 모델
+decoder_model = Model(
+    [decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
+    [decoder_outputs2] + [state_h2, state_c2])
+
+def decode_sequence(input_seq):
+    # 입력으로부터 인코더의 상태를 얻음
+    e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+     # <SOS>에 해당하는 토큰 생성
+    target_seq = np.zeros((1,1))
+    target_seq[0, 0] = tar_word_to_index['sostoken']
+
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition: # stop_condition이 True가 될 때까지 루프 반복
+
+        output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = tar_index_to_word[sampled_token_index]
+
+        if(sampled_token!='eostoken'):
+            decoded_sentence += ' '+sampled_token
+
+        #  <eos>에 도달하거나 최대 길이를 넘으면 중단.
+        if (sampled_token == 'eostoken'  or len(decoded_sentence.split()) >= (summary_max_len-1)):
+            stop_condition = True
+
+        # 길이가 1인 타겟 시퀀스를 업데이트
+        target_seq = np.zeros((1,1))
+        target_seq[0, 0] = sampled_token_index
+
+        # 상태를 업데이트 합니다.
+        e_h, e_c = h, c
+
+    return decoded_sentence
+
+# 원문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2text(input_seq):
+    temp=''
+    for i in input_seq:
+        if(i!=0):
+            temp = temp + src_index_to_word[i]+' '
+    return temp
+
+# 요약문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2summary(input_seq):
+    temp=''
+    for i in input_seq:
+        if((i!=0 and i!=tar_word_to_index['sostoken']) and i!=tar_word_to_index['eostoken']):
+            temp = temp + tar_index_to_word[i] + ' '
+    return temp
+
+for i in range(0, 149):
+    print("원문 : ",seq2text(encoder_input_test[i]))
+    print("실제 요약문 :",seq2summary(decoder_input_test[i]))
+    print("예측 요약문 :",decode_sequence(encoder_input_test[i].reshape(1, text_max_len)))
+    print("\n")
